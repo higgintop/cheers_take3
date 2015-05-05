689918738b48d65b6ac8d91783fef3b156372fa3
Allison Higginbotham <higgintop@gmail.com>
2015-05-05 15:31:52 -0500

    testing

--- lib/birthday_parser.rb
+++ lib/birthday_parser.rb
@@ -1,20 +1,8 @@
 #!/usr/bin/env ruby
-require 'date'
 
 class BirthdayParser
 
-  def self.parse(birthday_str)
-    todayYDay = Time.new.yday
-    # 2. need user's yday conversion
-    begin
-      userYDay = Date.strptime(birthday_str, "%m/%d").yday
-      if userYDay  >= todayYDay
-        return (userYDay-todayYDay).to_s
-      else
-        return ((365-todayYDay) + userYDay).to_s
-      end
-    rescue
-      return nil
-    end
+  def self.parse(birthday)
+    puts "Birthday!"
   end
 end
--- lib/cheer.rb
+++ lib/cheer.rb
@@ -6,43 +6,7 @@ require_relative 'birthday_parser'
 class Cheer
 
   def self.for_person(name)
-    output = ""
 
-    # remove whitespace
-    nameModified = name.gsub(/\s+/, "")
-
-    if nameModified.nil? or nameModified.empty?
-      return "I'd cheer for you, if only I knew who you were :(\n"
-    end
-
-    # remove special characters from nameModified
-    nameModified.gsub!(/[\d\W]/, "")
-
-    if nameModified.empty?
-      return "I'd cheer for you, if only I knew who you were :(\n"
-    end
-
-    an_letters = "AEFHILMNORSX"
-    nameModified.each_char do |letter|
-      if an_letters.include? letter.upcase
-        output << "Give me an.. " + letter.upcase + "!\n"
-      else
-        output << "Give me a... " + letter.upcase + "!\n"
-      end
-    end
-
-    output << "#{name.chomp}'s just GRAND!\n"
-  end
-
-  def self.for_birthday(birthday_str)
-    # first parse the birthday_str
-    num_days_until_bday = BirthdayParser.parse(birthday_str)
-
-    if num_days_until_bday.nil?
-      return "I'm sorry, I don't understand :( Try again next time.\n"
-    else
-      return "Awesome! Your birthday is in #{num_days_until_bday} day! Happy Birthday in advance!\n"
-    end
   end
 
 end
