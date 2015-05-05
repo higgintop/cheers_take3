22e13d55fdf81fbdd273abab2e1d02cd32198247
Allison Higginbotham <higgintop@gmail.com>
2015-05-05 13:59:53 -0500

    Done

--- test/units/test_birthday_parser.rb
+++ test/units/test_birthday_parser.rb
@@ -1,10 +1,24 @@
 require_relative '../helper'
-
+require_relative '../../lib/birthday_parser'
 
 class TestBirthdayParser < Minitest::Test
 
-  def test_fail
-    assert_equal true, false
+  def test_parse_for_valid_birthday_a
+    actual = BirthdayParser.parse("05/06")
+    expected = "1"
+    assert_equal expected, actual
+  end
+
+  def test_parse_for_valid_birthday_b
+    actual = BirthdayParser.parse("05/07")
+    expected = "2"
+    assert_equal expected, actual
+  end
+
+  def test_parse_for_invalid_birthday
+    actual = BirthdayParser.parse("33/01")
+    expected = nil
+    assert_equal expected, actual
   end
 
 end
--- test/units/test_cheer.rb
+++ test/units/test_cheer.rb
@@ -13,23 +13,67 @@ class TestCheer < Minitest::Test
   end
 
   def test_for_person_with_name_with_spaces
-    fail
+    actual = Cheer.for_person("Mary Jane")
+    expected = ""
+    expected << "Give me an.. M!\n" +
+                "Give me an.. A!\n" +
+                "Give me an.. R!\n" +
+                "Give me a... Y!\n" +
+                "Give me a... J!\n" +
+                "Give me an.. A!\n" +
+                "Give me an.. N!\n" +
+                "Give me an.. E!\n" +
+                "Mary Jane's just GRAND!\n"
+    assert_equal expected, actual
   end
 
   def test_for_person_with_name_thats_blank
-    fail
+    actual =  Cheer.for_person("")
+    expected = ""
+    expected << "I'd cheer for you, if only I knew who you were :(\n"
+    assert_equal expected, actual
   end
 
   def test_for_person_with_name_thats_all_whitespace
-    fail
+    actual =  Cheer.for_person("     ")
+    expected = ""
+    expected << "I'd cheer for you, if only I knew who you were :(\n"
+    assert_equal expected, actual
   end
 
   def test_for_person_with_name_containing_hyphen
-    fail
+    actual = Cheer.for_person("Mary-Jane")
+    expected = ""
+    expected << "Give me an.. M!\n" +
+                "Give me an.. A!\n" +
+                "Give me an.. R!\n" +
+                "Give me a... Y!\n" +
+                "Give me a... J!\n" +
+                "Give me an.. A!\n" +
+                "Give me an.. N!\n" +
+                "Give me an.. E!\n" +
+                "Mary-Jane's just GRAND!\n"
+    assert_equal expected, actual
   end
 
   def test_for_person_with_name_containing_non_word_chars
-    fail
+    actual =  Cheer.for_person("123#%")
+    expected = ""
+    expected << "I'd cheer for you, if only I knew who you were :(\n"
+    assert_equal expected, actual
   end
+
+  def test_for_birthday_with_valid_bday
+    actual = Cheer.for_birthday("05/06")
+    expected = "Awesome! Your birthday is in 1 day! Happy Birthday in advance!\n"
+    assert_equal expected, actual
+  end
+
+  def test_for_birthday_with_invalid_bday
+    actual = Cheer.for_birthday("33/02")
+    expected =  "I'm sorry, I don't understand :( Try again next time.\n"
+    assert_equal expected, actual
+  end
+
 
 end
